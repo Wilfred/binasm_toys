02bb8141b45d4c6cdd1341cdf88f8bd86823e43b
Wilfred Hughes <me@wilfred.me.uk>
2013-09-05 00:56:42 +0100

    Print ASCII data as hex

--- print_bytes.c
+++ print_bytes.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int c;
+    char buf[4];
     FILE *file;
+    size_t nread;
+
     file = fopen("ascii.txt", "r");
     if (file) {
-        while ((c = getc(file)) != EOF)
-            putchar(c);
+        while ((nread = fread(buf, 1, sizeof buf, file)) > 0) {
+            printf("0x%x%x%x%x %s\n", buf[0], buf[1], buf[2], buf[3], buf);
+
+            // fixme: what if the file doesn't fit in four byte blocks?
+        }
+        if (ferror(file)) {
+            printf("Error reading file!\n");
+        }
         fclose(file);
     }
     
