f83fc262eaab501038bfc981874912da67084072
Wilfred Hughes <me@wilfred.me.uk>
2013-09-05 23:50:08 +0100

    Don't print non-printable chars, handle any size file

--- print_bytes.c
+++ print_bytes.c
@@ -1,15 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void print_char(char c) {
+    // If the character is printable, print it. If not, print a dot.
+    if (c >= 32 && c < 127) {
+        printf("%c", c);
+    } else {
+        printf(".");
+    }
+}
 
 int main() {
-    char buf[4];
+    char *buf = malloc(4 * sizeof(char));
     FILE *file;
     size_t nread;
 
     file = fopen("ascii.txt", "r");
     if (file) {
-        while ((nread = fread(buf, 1, sizeof buf, file)) > 0) {
-            printf("0x%x%x%x%x %s\n", buf[0], buf[1], buf[2], buf[3], buf);
+        while ((nread = fread(buf, 1, 4 * sizeof(char), file)) > 0) {
 
+            printf("0x");
+
+            // Print the raw bytes.
+            switch (nread) {
+            case 1:
+                printf(" %02x         ", buf[0]);
+                break;
+            case 2:
+                printf(" %02x %02x      ", buf[0], buf[1]);
+                break;
+            case 3:
+                printf(" %02x %02x %02x   ", buf[0], buf[1], buf[2]);
+                break;
+            case 4:
+                printf(" %02x %02x %02x %02x", buf[0], buf[1], buf[2], buf[3]);
+                break;
+            }
+
+            printf("\t");
+
+            // Print as ASCII
+            if (nread >= 1) {
+                print_char(buf[0]);
+            }
+            if (nread >= 2) {
+                print_char(buf[1]);
+            }
+            if (nread >= 3) {
+                print_char(buf[2]);
+            }
+            if (nread >= 4) {
+                print_char(buf[3]);
+            }
+
+            printf("\n");
+            
             // fixme: what if the file doesn't fit in four byte blocks?
         }
         if (ferror(file)) {
