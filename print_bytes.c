ef350304194e1838f8df0b8f0c13f47468e37585
Wilfred Hughes <me@wilfred.me.uk>
2013-09-08 19:13:42 +0100

    Handle the case where the file doesn't exist.

--- print_bytes.c
+++ print_bytes.c
@@ -16,52 +16,56 @@ int main() {
     size_t nread;
 
     file = fopen("ascii.txt", "r");
-    if (file) {
-        while ((nread = fread(buf, 1, 4 * sizeof(unsigned char), file)) > 0) {
 
-            printf("0x");
-
-            // Print the raw bytes.
-            switch (nread) {
-            case 1:
-                printf(" %02x         ", buf[0]);
-                break;
-            case 2:
-                printf(" %02x %02x      ", buf[0], buf[1]);
-                break;
-            case 3:
-                printf(" %02x %02x %02x   ", buf[0], buf[1], buf[2]);
-                break;
-            case 4:
-                printf(" %02x %02x %02x %02x", buf[0], buf[1], buf[2], buf[3]);
-                break;
-            }
-
-            printf("\t");
-
-            // Print as ASCII
-            if (nread >= 1) {
-                print_char(buf[0]);
-            }
-            if (nread >= 2) {
-                print_char(buf[1]);
-            }
-            if (nread >= 3) {
-                print_char(buf[2]);
-            }
-            if (nread >= 4) {
-                print_char(buf[3]);
-            }
-
-            printf("\n");
-            
-            // fixme: what if the file doesn't fit in four byte blocks?
-        }
-        if (ferror(file)) {
-            printf("Error reading file!\n");
-        }
-        fclose(file);
+    if (file == NULL) {
+        printf("No such file.\n");
+        return 1;
     }
+    
+    while ((nread = fread(buf, 1, 4 * sizeof(unsigned char), file)) > 0) {
+
+        printf("0x");
+
+        // Print the raw bytes.
+        switch (nread) {
+        case 1:
+            printf(" %02x         ", buf[0]);
+            break;
+        case 2:
+            printf(" %02x %02x      ", buf[0], buf[1]);
+            break;
+        case 3:
+            printf(" %02x %02x %02x   ", buf[0], buf[1], buf[2]);
+            break;
+        case 4:
+            printf(" %02x %02x %02x %02x", buf[0], buf[1], buf[2], buf[3]);
+            break;
+        }
+
+        printf("\t");
+
+        // Print as ASCII
+        if (nread >= 1) {
+            print_char(buf[0]);
+        }
+        if (nread >= 2) {
+            print_char(buf[1]);
+        }
+        if (nread >= 3) {
+            print_char(buf[2]);
+        }
+        if (nread >= 4) {
+            print_char(buf[3]);
+        }
+
+        printf("\n");
+            
+        // fixme: what if the file doesn't fit in four byte blocks?
+    }
+    if (ferror(file)) {
+        printf("Error reading file!\n");
+    }
+    fclose(file);
     
     return 0;
 }
